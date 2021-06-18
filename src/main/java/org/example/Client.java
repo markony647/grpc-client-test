8c0c5d0521b64889edb055773321ffd5b71374f9
Vitalii Marchenko <external.Vitalii.Marchenko@de.bosch.com>
2021-06-18 16:49:51 +0300

    Add simple gRPC client

---
+++ src/main/java/org/example/Client.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import com.example.grpc.GreetingServiceGrpc;
+import com.example.grpc.GreetingServiceOuterClass;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+import java.util.Iterator;
+
+public class Client {
+
+    public static void main(String[] args) {
+        ManagedChannel channel = ManagedChannelBuilder.forTarget("localhost:8080")
+                .usePlaintext()
+                .build();
+
+        GreetingServiceGrpc.GreetingServiceBlockingStub stub = GreetingServiceGrpc.newBlockingStub(channel);
+
+        GreetingServiceOuterClass.HelloRequest request = GreetingServiceOuterClass.HelloRequest
+                .newBuilder()
+                .setName("Vitaly").build();
+
+        Iterator<GreetingServiceOuterClass.HelloResponse> helloResponse = stub.greeting(request);
+
+        while (helloResponse.hasNext()) {
+            System.out.println(helloResponse.next());
+        }
+
+        channel.shutdownNow();
+    }
+}
